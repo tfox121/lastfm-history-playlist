33e3ee1e5d95382f8bcf9ff91c14f4d612144543
Tom Fox <51032384+tfox121@users.noreply.github.com>
2023-02-05 18:39:50 -0500

    build: allow last.fm domain for images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['lastfm.freetls.fastly.net'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
