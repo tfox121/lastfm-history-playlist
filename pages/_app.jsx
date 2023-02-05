b0202557c944dc80324c88c97ad67d06de858e99
Tom Fox <51032384+tfox121@users.noreply.github.com>
2023-02-05 18:42:22 -0500

    feat: create initial username entry and track listing

---
+++ pages/_app.jsx
@@ -0,0 +1,28 @@
+import { ThemeProvider } from '@mui/material/styles';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import theme from '@/src/theme';
+
+export default function App({ Component, pageProps }) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        refetchOnmount: false,
+        refetchOnReconnect: false,
+        retry: false,
+        staleTime: 5 * 60 * 1000,
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
+}
